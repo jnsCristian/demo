2ab678a3f1f3d1dc0a66a0304f285085eda8a7b3
Cristian Stefan <stefan.cristian@nttdata.ro>
2017-09-06 12:51:42 +0300

    first commit

--- src/main/java/edu/Test.java
+++ src/main/java/edu/Test.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
 	@RequestMapping(path="test",method=RequestMethod.GET)
 	String test(){
-		return "test";
+		return "test1";
 	}
 }
